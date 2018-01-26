7875bc8832549a444ba53c2f7c01875efd98d6a6
Michael Mensinger <mikal815@gmail.com>
2018-01-25 20:02:45 -0800

    logging things to the console in main.js

--- js/main.js
+++ js/main.js
@@ -14,3 +14,20 @@ function change(){
 var name = 'Lucille'; // Store a string in a Javascript
 var numberOfWidgets = 10; // number
 var isCodingCool = true; // boolean
+console.log(name); //log the value of a var to the console
+
+//store an array in a var
+var characters = [50, true, 'Lucille', 'Michael Bluth', 'Tobias', 10, 2];
+console.log(characters[0]);
+
+var name = "dog"
+console.log(name);
+
+var numberOfCats = 40;
+console.log(numberOfCats);
+
+var iAmAlive = true;
+console.log(iAmAlive);
+
+var drinks = ['beer', 'wine', 'whiskey']
+console.log(drinks[2]);
